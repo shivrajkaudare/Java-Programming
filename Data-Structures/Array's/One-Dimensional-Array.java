3a7f77b8768fc01880c3fa29184e1acfc094b268
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-07-05 21:00:47 +0530

    Update One-Dimensional-Array.java

--- Data-Structures/Array's/One-Dimensional-Array.java
+++ Data-Structures/Array's/One-Dimensional-Array.java
@@ -1,32 +1,25 @@
 package Arrays;
-import java.util.Scanner;
-public class TwoDArray {
+import java.util.*;
+import java.lang.reflect.Array;
+
+public class OneDArray {
 
 	public static void main(String[] args) {
-	
-		int arr[][] = new int[3][3];
+		
+		int arr[] = new int[5];
 		
 		Scanner sc = new Scanner(System.in);
-		int i , j;
+		System.out.println("Enter Array Element");
 		
-		System.out.println("Enter Array Elements :");
-
-		for(i = 0; i < 3; i++) {
-			
-			for(j = 0; j < 3; j++) {
-				
-				arr[i][j] = sc.nextInt();
-			}
+		for(int i = 0; i < arr.length; i++) {
+			arr[i]= sc.nextInt();
 		}
-		System.out.println("The Array you have Entered is :");
-        for(i = 0; i < 3; i++) {
-			
-			for(j = 0; j < 3; j++) {
-				
-				System.out.print(arr[i][j]);
-			}
-			System.err.println();
-		}
+        System.out.println("The Array is : ");
+        
+        for(int i = 0; i < arr.length; i++) {
+        	
+	        System.out.println(arr[i]);
+        }
 	}
 
 }
