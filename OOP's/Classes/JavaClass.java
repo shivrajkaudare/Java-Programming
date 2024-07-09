5aff0e137fc790e7e43b0d3dfb7802e51d1e37b7
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-07-09 23:03:32 +0530

    Update JavaClass.java

--- OOP's/Classes/JavaClass.java
+++ OOP's/Classes/JavaClass.java
@@ -1,1 +1,26 @@
+package JavaClass;
 
+class Student{
+	int id;
+	String name;
+
+	public void inputes(int id, String name) {		
+		this.id = id;
+		this.name = name;
+		
+	}
+}
+
+public class JavaClass {
+
+	public static void main(String[] args) {
+		
+		Student s1 = new Student();
+		
+		s1.id = 101;
+		s1.name = "Shiv";
+		System.out.println("Student id "+ s1.id +" Student name " + s1.name);
+
+	}
+
+}
