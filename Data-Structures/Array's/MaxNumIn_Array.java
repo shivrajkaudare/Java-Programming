68d73f6479704aca11271a8a6e9e92c99cd92562
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-07-09 23:09:12 +0530

    Update MaxNumIn_Array.java

--- Data-Structures/Array's/MaxNumIn_Array.java
+++ Data-Structures/Array's/MaxNumIn_Array.java
@@ -1,1 +1,18 @@
+package Arrays;
 
+public class MaxNumInArray {
+
+	public static void main(String[] args) {
+		
+     int arr [] = {1,2,4,56,76};
+     int max = 0;
+     
+     for (int i = 0; i < arr.length; i++) {
+    	 if(arr[i] > max) {
+    		  max = arr[i];
+    	 }
+     }
+     System.out.println(max);
+	}
+
+}
