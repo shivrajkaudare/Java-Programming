b54524244b5de0a6982ab6505a10fa72f3be7228
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-07-10 10:41:18 +0530

    Update paramatrizedCon.java

--- OOP's/Constructors/paramatrizedCon.java
+++ OOP's/Constructors/paramatrizedCon.java
@@ -1,1 +1,17 @@
+package Constructors;
 
+public class paramatrizedCon {
+
+    String input;
+	public paramatrizedCon( String input) {
+		this.input = input;
+		System.out.println(input);
+		
+	}
+	public static void main(String[] args) {
+		
+		paramatrizedCon p1 = new paramatrizedCon("This is an Parametriesed Constructor");
+		
+	}
+
+}
