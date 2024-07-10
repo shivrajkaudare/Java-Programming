7ac0b573d34bb5b3ce3a7c89a9f4a6c301fe56ca
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-07-10 12:25:12 +0530

    Update Single_Inheritance.java

--- OOP's/Inheritance/Single_Inheritance.java
+++ OOP's/Inheritance/Single_Inheritance.java
@@ -1,1 +1,26 @@
+package OOPs;
 
+class Parent {
+	
+	void show() {
+		
+		System.out.println("This is parnet class");
+	}
+}
+class Child extends Parent{
+	void show2() {
+		super.show();
+		System.out.println("this is child class");
+	}
+}
+public class Inheritance {
+
+	public static void main(String[] args) {
+		
+		Child c1 = new Child();
+		c1.show2();
+		
+
+	}
+
+}
