7a862a0ff8e82c1d9df646781ba168104a2e18ce
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-07-10 11:08:44 +0530

    Update CopyCon.java

--- OOP's/Constructors/CopyCon.java
+++ OOP's/Constructors/CopyCon.java
@@ -1,1 +1,35 @@
+package Constructors;
+
+class Sample{
+	int id ;
+	String name;
+	int age;
+	int marks;
+	public Sample(int id, String name) {
+		this.id = id;
+		this.name = name;	
+	}
+	
+	public Sample(Sample s, int age, int marks) {
+		 id = s.id;
+		 name = s.name;
+		 this.age = age;
+		 this.marks = marks;	
+	}
+	
+	void show() {
+		System.out.println( id + " having name " + name + " and his age "+ age +" and his obtained marks are " + marks);
+	}
+}
+public class CopyCon {
+
+	public static void main(String[] args) {
+		
+          Sample s1 = new Sample(101, "Shivraj");
+         
+          Sample s2 = new Sample(s1, 23, 86);
+          s2.show();
+	}
+
+}
 
