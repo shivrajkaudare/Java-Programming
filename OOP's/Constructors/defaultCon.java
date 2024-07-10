5ee3787c8a0155ce007b7ef9282c675a35680a07
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-07-10 10:36:00 +0530

    Update defaultCon.java

--- OOP's/Constructors/defaultCon.java
+++ OOP's/Constructors/defaultCon.java
@@ -1,1 +1,18 @@
+package Constructors;
+
+class defaultConstructor{
+	public defaultConstructor(){
+		
+		System.out.println("This is Default Constructor");
+		
+	}
+}
+public class defaultCon {
+
+	public static void main(String[] args) {
+		
+          defaultConstructor d1 = new defaultConstructor();
+	}
+
+}
 
