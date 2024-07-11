99a43a2188dba4048e18f0a4dd9f03afece24ca4
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-07-11 20:06:28 +0530

    Update SumOf_RandomNum.java

--- Data-Structures/Numbers/SumOf_RandomNum.java
+++ Data-Structures/Numbers/SumOf_RandomNum.java
@@ -1,1 +1,34 @@
+package Other_Programs;
+
+import java.util.Scanner;
+
+public class SumAvgOfNumbers {
+
+	public static void main(String[] args) {
+		
+		// Sum of an Random Numbers.
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int sum = 0;
+		int n;
+		System.out.println("How many Elements sum and Avg you want to count : ");
+		n = sc.nextInt();
+		
+		System.out.println("Enter elements");
+		 for(int i = 0; i < n; i++) {
+			 int num = sc.nextInt();
+			  sum = sum + num;
+		 }
+		 System.out.println("Sum of Numbers you have entered = "+sum);
+		 
+	     double avg = sum / n;
+	     System.out.print("Average of numbers you have entered = "+ avg);
+	     
+	     sc.close();
+	     
+	     	
+	}
+
+}
 
