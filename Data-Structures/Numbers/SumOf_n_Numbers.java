098cc879af5783744bf7a0de4e5c4da8c93c8938
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-07-11 20:45:15 +0530

    Create SumOf_n_Numbers.java

---
+++ Data-Structures/Numbers/SumOf_n_Numbers.java
@@ -0,0 +1,24 @@
+package Other_Programs;
+
+import java.util.Scanner;
+
+public class SumOf_n_Numbers {
+	public static void main(String [] args) {
+		
+		Scanner sc = new Scanner(System.in);
+	    System.out.print("Enter the nth Element = ");
+		int n = sc.nextInt();
+		
+		int sum = 0;
+		for(int i = 0; i <= n; i++) {
+			
+			sum = sum + i;
+		}
+		System.out.println("Sum of n numbers is = "+ sum);
+		
+		double avg = sum / n;
+		System.out.print("Avg of sum is = "+ avg);
+		
+	}
+
+}
