a02ad98de8b391a57ac489e099051ddbfae40e3b
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-07-11 15:30:22 +0530

    Update Factorial.java

--- Data-Structures/Numbers/Factorial.java
+++ Data-Structures/Numbers/Factorial.java
@@ -1,1 +1,21 @@
+package Other_Programs;
 
+import java.util.Scanner;
+
+public class Factorial {
+	public static void main(String args[]){
+		
+		int fact = 1;
+		int num;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		num = sc.nextInt();
+		
+		for(int i = 1; i <=num; i++) {
+			fact *= i;
+		}
+		System.out.println("factorial of nuber is "+ fact);
+		
+	}
+
+}
