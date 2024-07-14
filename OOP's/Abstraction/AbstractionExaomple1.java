b27f8e5acfaedb0b19ffbec0a4eba5af3261088c
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-07-14 18:38:49 +0530

    Update AbstractionExaomple1.java

--- OOP's/Abstraction/AbstractionExaomple1.java
+++ OOP's/Abstraction/AbstractionExaomple1.java
@@ -1,1 +1,26 @@
-
+ abstract class demo
+ {
+ }
+ abstract void payment();
+ class wallet extends demo{
+ void payment() 
+{
+ System.out.println("Wallet payment success");
+ }
+ }
+ class netbanking extends demo{
+ void payment()
+ {
+ System.out.println("Netbanking payment success");
+ }
+ }
+class AbstractionExaomple1.java
+ { 
+public static void main(String args[])
+ {  
+wallet w=new wallet();
+ w.payment();
+ netbanking n=new netbanking();
+ n.payment();
+ }  
+}
