77daa37f68e0c54ed2981751f29c11bb9464e29b
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-07-15 18:06:43 +0530

    Update AggregationExample.java

--- OOP's/Aggregation/AggregationExample.java
+++ OOP's/Aggregation/AggregationExample.java
@@ -1,1 +1,21 @@
-
+public class Employee
+ {
+ Stringname;
+ Address address;
+ publicEmp(Stringname, Addressaddress){
+ this.name = name;
+ this.address = address;
+ }
+ void display()
+ {
+ System.out.println(name);
+ System.out.println(address.state);
+ System.out.println(address.city);
+ }
+ public static void main(String[]args)
+ {
+ Addressa1 = newAddress(“MH","Pune");
+ Employee e1 = newEmployee(“Abc",a1);
+ e1.display();
+ }
+ }
