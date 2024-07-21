7d7a3a58cbd2b37f52c4fe0123adb3fcb3fb84d1
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-07-21 19:56:05 +0530

    Update StaticVariable.java

--- Keywords/Static/StaticVariable.java
+++ Keywords/Static/StaticVariable.java
@@ -1,1 +1,30 @@
+// Static Variable :-
+// - The static variable can be used to refer to the common property of all objects.
+// - The static variable gets memory only once in the class area at the time of class loading.
 
+class Emp_stat
+ {
+    intid;
+    Stringname;
+    static String company_name="Wipro";
+    Emp_stat(intid,Stringname)
+ {
+    this.id=id;
+    this.name=name;
+ }
+ public void emp_details()
+ {
+    System.out.println("Id="+id);
+    System.out.println("Name="+name);
+    System.out.println("Company name="+company_name);
+ }
+ } 
+public class static_variable
+ {
+ public static void main(String[] args) 
+{
+     Emp_state=new Emp_stat(1,"abc");
+     e.emp_details();
+     Emp_state1=new Emp_stat(2,"pqr");
+     e1.emp_details();
+ }
