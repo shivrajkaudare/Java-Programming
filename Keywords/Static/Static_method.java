79966bd00310e3f8fdb60ca4589d03b70e077430
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-07-22 19:29:08 +0530

    Update Static_method.java

--- Keywords/Static/Static_method.java
+++ Keywords/Static/Static_method.java
@@ -1,1 +1,32 @@
+class Abcd
+ {
+    intid;
+    Stringname;
+    static String company_name="Wipro";
+ static void display()
+ {
+    System.out.println("I am static method");
+    company_name="amdocs";
+ }
+ Abcd(intid,Stringname)
+ {
+    this.id=id;
+    this.name=name;
+ }
 
+ public void emp_details()
+ {
+    System.out.println("Id="+id);
+    System.out.println("Name="+name);
+    System.out.println("Company name="+company_name);
+ }
+ } 
+public class static_variable
+ {
+ public static void main(String[] args) 
+{
+    Abcd.display();
+    Abcda=new Abcd(1,"abcd");
+    a.emp_details();
+ }
+ }
