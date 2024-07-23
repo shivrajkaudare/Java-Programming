1ec4857792eea1bfccfdd07266235d38524d2776
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-07-23 21:09:06 +0530

    Update StaticVariable.java

--- Keywords/Static/StaticVariable.java
+++ Keywords/Static/StaticVariable.java
@@ -4,27 +4,27 @@
 
 class Emp_stat
  {
-    intid;
-    Stringname;
-    static String company_name="Wipro";
-    Emp_stat(intid,Stringname)
+    int id;
+    String name;
+    static String company_name = "Wipro";
+ Emp_stat(int id, String name)
  {
-    this.id=id;
-    this.name=name;
+    this.id = id;
+    this.name = name;
  }
  public void emp_details()
  {
-    System.out.println("Id="+id);
-    System.out.println("Name="+name);
-    System.out.println("Company name="+company_name);
+    System.out.println("Id = "+id);
+    System.out.println("Name = "+name);
+    System.out.println("Company name = "+company_name);
  }
  } 
 public class static_variable
  {
  public static void main(String[] args) 
 {
-     Emp_state=new Emp_stat(1,"abc");
+     Emp_state = new Emp_stat(1,"abc");
      e.emp_details();
-     Emp_state1=new Emp_stat(2,"pqr");
+     Emp_state1 = new Emp_stat(2,"pqr");
      e1.emp_details();
  }
