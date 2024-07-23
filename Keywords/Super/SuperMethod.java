887f7cd026841b24952e5fe4abce9a50c252bc6c
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-07-23 21:27:37 +0530

    Update SuperMethod.java

--- Keywords/Super/SuperMethod.java
+++ Keywords/Super/SuperMethod.java
@@ -1,1 +1,22 @@
+// Used to invoke parent class method
+// It should be used if subclass contains the same method as parent class.
+// It is used if method is overridden
 
+ class demo
+ {
+ void show()
+ {
+ System.out.println("Super class method");
+ }
+ }
+ class sample extends demo{
+ void show()
+ {
+ super.show();
+ System.out.println("Sub class method");
+ } 
+public static void main(String args[]) {  
+sample s1 = new sample();
+ s1.show();
+ }  
+} 
