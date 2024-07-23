2f2c603e2206ced9c32305ac8a8c50066308e447
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-07-23 21:28:58 +0530

    Update Super_Constructor.java

--- Keywords/Super/Super_Constructor.java
+++ Keywords/Super/Super_Constructor.java
@@ -1,1 +1,21 @@
+// Used to invoke the parent class constructor
+// Note: super() is added in each class constructor automatically by compiler if there is no super()
 
+
+ class demo
+ {
+ demo()
+ {
+ System.out.println("Super constructor");
+ }
+ }
+ class sample extends demo{ 
+sample()
+ {
+ super();
+ System.out.println("Sub class constructor");
+ }
+ public static void main(String args[]) {  
+sample s1 = new sample();
+ }  
+} 
