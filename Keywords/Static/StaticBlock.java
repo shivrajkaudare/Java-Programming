5bacb8c62f760b93349d070ee889043a111f184c
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-07-23 21:16:24 +0530

    Update StaticBlock.java

--- Keywords/Static/StaticBlock.java
+++ Keywords/Static/StaticBlock.java
@@ -1,1 +1,18 @@
+ // Is used to initialize the static data member or initialised static fiels in java.
+//  It is executed before the main method at the time of classloading.
 
+class demo {
+  static
+  {
+    System.out.println("This is an Static block"); 
+  }
+  public void display(){
+    System.out.println("Display Method");
+  }
+}
+
+public static void main (String [] args){
+
+  demo d1 = new demo();
+  d1.display();
+}
