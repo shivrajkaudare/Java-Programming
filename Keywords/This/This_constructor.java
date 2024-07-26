12d4dd48973db5dd422c62509d7fce552f996613
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-07-26 15:52:25 +0530

    Update This_constructor.java

--- Keywords/This/This_constructor.java
+++ Keywords/This/This_constructor.java
@@ -1,1 +1,19 @@
+// it is used to reuse the constructor.
+class demo
+{
+ demo()
+  {
+      System.out.println("I am demo constructor");
+  } 
 
+ demo(int a)
+  {
+     this();
+     System.out.println("A="+a);
+  } 
+  
+class sample { 
+  public static void main(String args[]) { 
+     demo d=new demo(10); 
+  } 
+}
