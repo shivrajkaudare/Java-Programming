d1dae5b8434494aad7869f1aba06a13227a675ae
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-07-27 12:39:59 +0530

    Update This_Method.java

--- Keywords/This/This_Method.java
+++ Keywords/This/This_Method.java
@@ -1,24 +1,17 @@
-//The this keyword can be used to refer current class instance variable.
-
-class sample{ 
-         int id;
-         String name; 
-
-  sample(int id, String name) {
-         this.id = id; 
-         this.name = name; 
-     }
-
-  void show() {
-
-         System.out.println("Id="+id);
-         System.out.println("Name="+name);
-    }
-class This_Method { 
-public static void main(String args[]) { 
-
-         demo d=new demo(); 
-         d.method2();
+// If you don't use the this keyword, compiler automatically adds this keyword while invoking the method.
+class demo {
+  void method1() {
+        System.out.println("I am method 1");
   } 
-} 
+  void method2() {
+        System.out.println("I am method 2");
+        this.method1();
+  } 
+}
+class sample { 
+public static void main(String args[]) { 
+     demo d=new demo(); 
+     d.method2();
+  } 
 
+}
