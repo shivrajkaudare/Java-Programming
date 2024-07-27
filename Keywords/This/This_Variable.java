6f5c7f27adc66abaaee2862c2fa3c70a7ef3161a
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-07-27 12:32:47 +0530

    Update This_Variable.java

--- Keywords/This/This_Variable.java
+++ Keywords/This/This_Variable.java
@@ -1,1 +1,22 @@
 
+// The this keyword can be used to refer current class instance variable.
+
+class sample{ 
+          int id;
+          String name; 
+
+   sample(int id, String name) {
+          this.id = id; 
+          this.name = name; 
+   }
+   void show() {
+          System.out.println("Id="+id);
+          System.out.println("Name="+name);
+   }
+  public static void main(String args[]) { 
+
+         sample m1=new sample(101,"abc"); 
+         m1.show();
+    } 
+
+}
