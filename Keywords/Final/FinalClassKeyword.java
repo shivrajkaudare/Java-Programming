82f7e81c4ea5c66f61f438e8a3eafd9aa8a39530
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-07-28 14:43:47 +0530

    Update FinalClassKeyword.java

--- Keywords/Final/FinalClassKeyword.java
+++ Keywords/Final/FinalClassKeyword.java
@@ -1,1 +1,13 @@
 
+// If you make any class as final, you cannot extend it.
+
+final class Parent
+{ }
+
+public class sample extends Parent {
+   public static void main(String[] args) {
+
+       System.out.println("Hello world");
+  }
+
+}
