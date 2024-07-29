8920f1bcc668768fdbe9e965ed672f76fefd2cee
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-07-29 18:05:31 +0530

    Update FinalMethod.java

--- Keywords/Final/FinalMethod.java
+++ Keywords/Final/FinalMethod.java
@@ -1,1 +1,15 @@
-
+// If you make any method as final, you cannot override it.
+ class Parent {  
+   final void display() {
+         System.out.println("final method");
+   }  
+ }
+ public class sampleextends Parent {
+   void display() {
+        System.out.println("final method");
+   } 
+ public static void main(String[] args) {
+       sample s=new sample();  
+       s.display();
+  }
+ }
