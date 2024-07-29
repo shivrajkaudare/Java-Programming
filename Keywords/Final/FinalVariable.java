0ff183343e08d50d2b7c2c046acfa75b64b4adfe
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-07-29 18:07:18 +0530

    Update FinalVariable.java

--- Keywords/Final/FinalVariable.java
+++ Keywords/Final/FinalVariable.java
@@ -1,1 +1,12 @@
+// If you make any variable as final, you cannot change the value of final variable(It will be constant)
+ public class sample {
+    final inta=10;//final variable  
+    void display() {  
+      a=20;  //error
+    }  
+public static void main(String[] args) {
+    sample s=new sample();  
+    s.display();
+  }
+ }
 
