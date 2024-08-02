a9f268658db2f52ba1978b73f535ec3392ac1ac9
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-08-02 19:43:31 +0530

    Update Hierarchical_Inheritance.java

--- OOP's/Inheritance/Hierarchical_Inheritance.java
+++ OOP's/Inheritance/Hierarchical_Inheritance.java
@@ -1,1 +1,32 @@
+package OOPs;
 
+class College {
+	 String Cname = "Dr. DY Patil College Akurdi";
+
+	    public String CollegeData() {
+	        return Cname;
+	    }
+}
+class Stud1 extends College{
+  public void Stud1Data(String Name){
+    System.out.println(" Student Name is : "+ Name);
+    System.out.println(" Students College Name is : "+ super.CollegeData());
+  }
+}
+class Stud2 extends College{
+  public void Stud2Data(String Name){
+	  
+    System.out.println(" Student Name is : "+ Name);
+    System.out.println(" Students College Name is : "+ super.CollegeData());
+  }
+}
+
+public class Hierarchical_inheritance{
+  public static void main(String [] args){
+    Stud1 s1 = new Stud1();
+    s1.Stud1Data("Shivraj Kaudare");
+
+    Stud2 s2 = new Stud2();
+    s2.Stud2Data("Nikeeta Kaudare");
+  }
+}
