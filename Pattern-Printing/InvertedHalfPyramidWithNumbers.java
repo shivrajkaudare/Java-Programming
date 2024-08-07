66d971b83b3e7ebefbd03f005b2c15b5f7c077b8
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-08-07 22:15:38 +0530

    Update InvertedHalfPyramidWithNumbers.java

--- Pattern-Printing/InvertedHalfPyramidWithNumbers.java
+++ Pattern-Printing/InvertedHalfPyramidWithNumbers.java
@@ -1,10 +1,10 @@
 package Pattern_Printing;
 
 // Output - 1 2 3 4 5
-//            1 2 3 4
-//              1 2 3
-//                1 2
-//                  1
+//          1 2 3 4
+//          1 2 3
+//          1 2
+//          1
 public class InvertedHalfPyramidWithNumbers {
 
 	public static void main(String[] args) {
