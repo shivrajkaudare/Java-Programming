d91dbc5ba3540e137b72d02254e1585da4eaf6e6
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-08-08 21:16:06 +0530

    Update LinkedList.java

--- Collections/List/LinkedList.java
+++ Collections/List/LinkedList.java
@@ -1,1 +1,19 @@
+// LinkedList :-
+// it can contain duplicate elements.
+// it maintain insertion order.
+// it allows random access because it works on index basis.
+// manupulation is little is slower because lots of bit shifting requires if any element is removed from array list.
+import java.util.*;
 
+public class ArrayListEx {
+  public static void main(String args []) {
+
+     ArrayList<String> li = new ArrayList<String>;
+            li.add("Shivraj");
+            li.add("Nikeeta");
+            li.add("Swapnil");
+            li.add("Deepak");
+    System.out.println(list);
+    
+  }
+}
