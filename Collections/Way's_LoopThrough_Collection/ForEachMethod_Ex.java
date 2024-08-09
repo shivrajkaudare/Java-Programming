aa97e66ca603b68aa6a8be4365dd6dad7637c010
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-08-09 20:10:54 +0530

    Update ForEachMethod_Ex.java

--- Collections/Way's_LoopThrough_Collection/ForEachMethod_Ex.java
+++ Collections/Way's_LoopThrough_Collection/ForEachMethod_Ex.java
@@ -1,1 +1,20 @@
+import java.util.*;
 
+// for Each method with lambda expression is used to loop through Collection's.
+// ex. list , map, set .
+
+public class IteratorEx {
+      public static void main(String args[]){
+
+          LinkedList<String> li = new LinkedList<String>();
+                  li.add("Shiv");
+                  li.add("Nikeeta");
+                  li.add("Deepak");
+                  li.add("Shivraj");
+
+        forEach(elements -> {
+            System.out.println(elements);
+          
+        )};
+      }
+}
