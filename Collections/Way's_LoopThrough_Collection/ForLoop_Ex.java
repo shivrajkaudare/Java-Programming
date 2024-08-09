764cab8ce1fcd44d025f6605a07acc70b6b1a8d0
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-08-09 20:02:42 +0530

    Update ForLoop_Ex.java

--- Collections/Way's_LoopThrough_Collection/ForLoop_Ex.java
+++ Collections/Way's_LoopThrough_Collection/ForLoop_Ex.java
@@ -1,1 +1,20 @@
+import java.util.*;
 
+// For loop is used to loop through Collection's.
+// ex. list , map, set .
+
+public class IteratorEx {
+      public static void main(String args[]){
+
+          LinkedList<String> li = new LinkedList<String>();
+                  li.add("Shiv");
+                  li.add("Nikeeta");
+                  li.add("Deepak");
+                  li.add("Shivraj");
+
+          for(String elements : li){
+            
+            System.out.println(elements);
+          }
+      }
+}
