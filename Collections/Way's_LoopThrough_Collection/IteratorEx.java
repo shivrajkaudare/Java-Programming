8887253bce6211483f2c5b09aa185d0657b134c5
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-08-09 19:15:01 +0530

    Update IteratorEx.java

--- Collections/Way's_LoopThrough_Collection/IteratorEx.java
+++ Collections/Way's_LoopThrough_Collection/IteratorEx.java
@@ -1,1 +1,23 @@
+import java.util.*;
 
+// Iterator it is object that used to loop through Collection's.
+// ex. list , map, set .
+
+public class IteratorEx {
+      public static void main(String args[]){
+
+          LinkedList<String> li = new LinkedList<String>();
+                  li.add("Shiv");
+                  li.add("Nikeeta");
+                  li.add("Deepak");
+                  li.add("Shivraj");
+
+        // Iterator for print list elements
+        Iterator<String> itr = li.Iterator();
+
+        while(itr.hasNext()) {
+
+          System.out.println(itr.next());
+      }
+}
+  
