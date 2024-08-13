217d151999cc11c540f3fec690fc9f79e5623724
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-08-13 22:02:58 +0530

    Create DecrementingNumericPyramid.java

---
+++ Pattern-Printing/DecrementingNumericPyramid.java
@@ -0,0 +1,36 @@
+package Pattern_Printing;
+
+// Output - 00000000010
+//          000000009
+//          00000008
+//          0000007
+//          000006
+//          00005
+//          0004
+//          003
+//          02
+//          1
+
+
+public class DecrementingNumericPyramid {
+
+    public static void main(String[] args) {
+        
+        int n = 10;
+        
+        for(int i = 0; i < n; i++) {
+            
+            // Printing leading zeros
+            for(int j = 0; j < n-i-1; j++) {
+                System.out.print(0);
+            }
+            
+            // Handling the special case for the first number to print 10 instead of just 9
+            if (i == 0) {
+                System.out.println(10);
+            } else {
+                System.out.println(n-i);
+            }
+        }
+    }
+}
