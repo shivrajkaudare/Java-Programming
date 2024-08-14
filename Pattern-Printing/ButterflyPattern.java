7f016cfc365beff8e0dcafbf0e3927a03dd9d195
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-08-14 22:55:17 +0530

    Update ButterflyPattern.java

--- Pattern-Printing/ButterflyPattern.java
+++ Pattern-Printing/ButterflyPattern.java
@@ -1,4 +1,4 @@
-
+package Pattern_Printing;
 
 // Output - *        *
 //          **      **
@@ -8,10 +8,57 @@
 //          ***    ***
 //          **      **
 //          *        *
+
 public class ButterflyPattern {
 
-    public static void main(String[] args) {
-        
-    }
-    
+	public static void main(String[] args) {
+		
+		int n = 4;
+		int m = 8;
+		
+		// 1st half part.
+		
+		for(int i = 1; i <= n; i++) {
+			
+			for(int j = 1; j <= i; j++) {
+				
+				System.out.print("*");
+			}
+			// for loop for the spaces.
+			for(int j = 1; j <= 2*(n-i); j++) {
+				
+				System.out.print(" ");
+				
+			}
+				
+            for(int j = 1; j <= i ; j++) {
+				
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+		
+		// 2nd half part.
+		
+		for(int i = n; i >= 1; i--){
+			
+			for(int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			// for loop for the spaces.
+			for(int j = 1; j <= 2*(n-i); j++) {
+				
+				System.out.print(" ");
+			}
+			for(int j = 1; j <= i; j++) {
+				
+				System.out.print("*");
+			}
+			System.out.println();
+			
+		}
+		
+	}
+
 }
