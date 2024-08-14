0f5abd72ff52240f959a899e2e3ef540cf88ea6d
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-08-14 22:20:41 +0530

    Create ButterflyPattern.java

---
+++ Pattern-Printing/ButterflyPattern.java
@@ -0,0 +1,19 @@
+
+
+// Output - *        *
+//          **      **
+//          ***    ***
+//          ****  ****
+//          **********
+//          **********
+//          ****  ****
+//          ***    ***
+//          **      **
+//          *        *
+public class ButterflyPattern {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
