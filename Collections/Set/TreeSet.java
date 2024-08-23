0eb20d375e35b75e10d2712731be2e26d2a7ec3e
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-08-23 19:06:31 +0530

    Update TreeSet.java

--- Collections/Set/TreeSet.java
+++ Collections/Set/TreeSet.java
@@ -1,1 +1,28 @@
+// TreeSet class contains unique elements only like HashSet.
 
+// TreeSet class access and retrieval times are quiet fast.
+
+// TreeSet class doesn't allow null element.
+
+// TreeSet class maintains ascending order.
+
+import java.util.*;
+
+public class sample { 
+
+    public static void main(String args[]) { 
+
+          TreeSet<String> al=new TreeSet<String>(); 
+                    al.add("Liam"); 
+                    al.add("Noah"); 
+                    al.add("Oliver"); 
+                    al.add("Elijah"); 
+
+          Iterator<String> itr = al.iterator();
+          while(itr.hasNext()) { 
+
+                 System.out.println(itr.next()); 
+          } 
+      } 
+
+}
