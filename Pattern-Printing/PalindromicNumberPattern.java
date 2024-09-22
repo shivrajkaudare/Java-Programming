429b5043f405ac100314c19cd23eea2f785bea5e
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-09-22 20:23:45 +0530

    Create PalindromicNumberPattern.java

---
+++ Pattern-Printing/PalindromicNumberPattern.java
@@ -0,0 +1,27 @@
+// Output -   1
+//          2 1 2
+//        3 2 1 2 3
+//      4 3 2 1 2 3 4
+//    5 4 3 2 1 2 3 4 5
+
+public class PalindromicNumberPattern {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i = 1; i <= n; i++){
+            // for loop for spaces.
+            for(int j = 1; j<=n-i; j++){
+                System.out.print("");
+            }
+            // 1st half.
+            for(int j = i; j >= 1; j--){
+                System.out.print(j);
+            }
+            // 2nd half.
+            for(int j = 2; j <= i; j++){
+                   System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    
+}
