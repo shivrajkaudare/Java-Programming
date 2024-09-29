fe80000c4254a81011e962ecd396120465393602
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-09-29 19:51:51 +0530

    Update LinkedListEX.java

--- Data-Structures/LinkedList/LinkedListEX.java
+++ Data-Structures/LinkedList/LinkedListEX.java
@@ -1,1 +1,25 @@
+// LinkedList -
+// - it can Contain Duplicate elements.
+// - sequential access, so it is slower to accessing the elements compare to ArrayList.
+// - insertion and deletion of elements is faster.
+// - LinkedLis's are Synchronized.
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class LinkedListEx{
+
+	public static void main(String[] args) {
+		
+		LinkedList<String> li = new LinkedList<String>();
+		
+		    li.add("Shiv");
+		    li.add("Raj");
+		    li.add("Shivraj");
+		    li.add("Deepak");
+		    
+		   System.out.println(li);
+		
+	}
+
+}
 
