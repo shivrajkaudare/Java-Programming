bf2b0a3e70507389680992d6d736153da6b10489
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-09-30 17:26:36 +0530

    Update PriorityQueueEx.java

--- Collections/Queue/PriorityQueueEx.java
+++ Collections/Queue/PriorityQueueEx.java
@@ -1,1 +1,26 @@
+// Priority Queue is used when the objects when the objects are supposed to be processed based on priority.
+// it does not permits null elements.
+// we can not create priority queue of elements.
+// non-synchronised and not thread safe.
+// time complexity for add and poll(remove first) elements is O(log(n)).
 
+import java.util.*;
+public class PriorityQueueEx{
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        pq.add(1);
+        pq.add(2);
+        pq.add(3);
+        pq.add(4);
+
+        System.out.println(pq);
+
+        // find the top element of priority queue.
+        System.out.println(pq.peek());
+
+        // remove top element of priority queue.
+
+        System.out.println(pq.poll());
+    }
+}
