41085e84f849b2dba814eaf310dd12e76ce68b00
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-10-18 09:38:27 +0530

    Update PrimeNumber2.java

--- Data-Structures/Numbers/PrimeNumber2.java
+++ Data-Structures/Numbers/PrimeNumber2.java
@@ -1,31 +1,32 @@
 // program- print the prime numbers in given range.
 
 import java.util.Scanner;
-
-public class PrimeNumber2 {
-	public static void main(String  args[]) {
-		
-		Scanner sc = new Scanner(System.in);
-		  System.out.println("Enter a last element ");
-		  int n = sc.nextInt();
-		  
-		 
-		  for(int i = 2; i <= n; i++) {
-			  boolean isPrime = true;
-			  
-		     for(int j = 2; j < i/2; j++) {
-			  
-			  if( i % j == 0) {
-				  isPrime = false; 
-				   break;
-			  }  
-		  }
-		  if(isPrime) {
-			  System.out.println(i);
-			  
-		  }
-		  }
-	}
-
+class PrimeNumber {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter a Number");
+       int n = sc.nextInt();
+       
+      if(n <= 1){
+          System.out.println("Enter a number greater than 1");
+      }else{
+           System.out.print("The Prime numbers till given Numbers are : ");
+		   
+          for(int i = 2; i <=n; i++){
+              boolean isPrime = true;
+              for(int j = 2; j <= i/2; j++){
+                  
+                  if(i % j == 0){
+                      isPrime = false;
+                      break;
+                  }
+              }
+              if(isPrime){
+                  System.out.print(i+",");
+              }
+          }
+         
+      }
+      
+    }
 }
-
