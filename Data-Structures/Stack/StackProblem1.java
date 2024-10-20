5f5e2d34d415cce656660cb1c61d19d86982e3b2
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-10-20 21:44:35 +0530

    Update StackProblem1.java

---
+++ Data-Structures/Stack/StackProblem1.java
@@ -0,0 +1,14 @@
+// Ex. - Push element at the bottom of the Stack.
+import java.util.Stack;
+public class StackProblem1 {
+    public static void pushBottom(int data, Stack<Integer> s){
+
+    }
+    public static void main(){
+
+        Stack<Integer> s = new Stack<Integer>();
+        
+
+    }
+    
+}
