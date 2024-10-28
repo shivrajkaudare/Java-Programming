5028b3e3d0071252ce4525d2880e97e0b74b1ccf
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-10-28 11:52:08 +0530

    Update StringBufferExample.java

--- Data-Structures/Strings/StringBufferExample.java
+++ Data-Structures/Strings/StringBufferExample.java
@@ -1,1 +1,22 @@
+// StringBuffer() - it is class in java that represents mutable sequence of character(String).
+//                - it allow us to modify content of string without creating object every time.
+//                - thread safe can be shared in multiple threads.
+//                - it is used when thread safty is required.
+//                - it is synchronised so low performance.
+//                - append() - use to instert character at the end of the String buffer.
+//                - insert() - insert the character at the specified posistion.
+//                - delete() - used tp remove character from the buffer.
+//                - reverse() - used to reverse order of the characters in the buffer.
+public class StringBufferExample{
+    public static void main(String[] args) {
+        StringBuffer str = new StringBuffer("Hello");
+                str.append(" ");
+                str.append("MY");
+                str.append(" ");
+                str.append("is");
+                str.append(" ");
+                str.append("Shivraj");
 
+                System.out.println(str);
+    }
+}
