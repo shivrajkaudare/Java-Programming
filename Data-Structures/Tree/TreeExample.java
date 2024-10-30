48589180ebf80492d5a5a77d72ca4771202a5204
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-10-30 19:36:32 +0530

    Update TreeExample.java

--- Data-Structures/Tree/TreeExample.java
+++ Data-Structures/Tree/TreeExample.java
@@ -1,3 +1,7 @@
+// - Tree data structure is a hierarchical structure that is used to represent and organize data in a way that is easy to navigate and search.
+// - It is a collection of nodes that are connected by edges and has a hierarchical relationship between the nodes. 
+// - 
+
 public class TreeExample {
         public static void main(String[] args) {
             
