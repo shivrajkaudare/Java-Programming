6553109265caa854ca508e4089d87bd133ccef03
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-12-03 18:56:32 +0530

    Update SecondLargestNumber.java

--- Data-Structures/Array's/SecondLargestNumber.java
+++ Data-Structures/Array's/SecondLargestNumber.java
@@ -1,1 +1,33 @@
+// Java program to find second largest element in an array
+// using Sorting
 
+import java.util.Arrays;
+
+class SecondLargestNumber {
+    
+    // function to find the second largest element
+    static int getSecondLargest(int[] arr) {
+        int n = arr.length;
+        
+        // Sort the array in non-decreasing order
+        Arrays.sort(arr);
+        
+        // start from second last element as last element is the largest
+        for (int i = n - 2; i >= 0; i--) {
+            
+            // return the first element which is not equal to the 
+            // largest element
+            if (arr[i] != arr[n - 1]) {
+                return arr[i];
+            }
+        }
+        
+        // If no second largest element was found, return -1
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 12, 35, 1, 10, 34, 1 };
+        System.out.println(getSecondLargest(arr));
+    }
+}
