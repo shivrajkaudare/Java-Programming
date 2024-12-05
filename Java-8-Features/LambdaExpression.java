c761b8c9af1fce5991156aac718e5a1353104e1a
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-12-05 20:31:49 +0530

    Update LambdaExpression.java

--- Java-8-Features/LambdaExpression.java
+++ Java-8-Features/LambdaExpression.java
@@ -1,1 +1,14 @@
+// Lambda expression.
+import java.util.ArrayList;
 
+public class Main {
+  public static void main(String[] args) {
+    ArrayList<Integer> numbers = new ArrayList<Integer>();
+    numbers.add(5);
+    numbers.add(9);
+    numbers.add(8);
+    numbers.add(1);
+    // print the Array list using lambda expression.
+    numbers.forEach( (n) -> { System.out.println(n); } );
+  }
+}
