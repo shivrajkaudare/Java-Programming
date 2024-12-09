3f6b1c60ef05ef1a74c817b29c302ba841901614
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-12-09 19:36:07 +0530

    Update RemoveDuplicateFromArray.java

--- Data-Structures/Array's/RemoveDuplicateFromArray.java
+++ Data-Structures/Array's/RemoveDuplicateFromArray.java
@@ -1,1 +1,19 @@
+// Remove Duplicates from a Sorted Array.
+class RemoveDuplicates {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0; // Handle empty array
+        }
 
+        int i = 0; // Pointer for unique elements
+        
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;            // Move to the next unique position
+                nums[i] = nums[j]; // Update the position with the new unique value
+            }
+        }
+        
+        return i + 1; // Number of unique elements
+    }
+}
