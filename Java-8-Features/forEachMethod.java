00c3a709cf4ec9e92be29865211cd1c84953b60a
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-12-17 19:41:20 +0530

    Update forEachMethod.java

--- Java-8-Features/forEachMethod.java
+++ Java-8-Features/forEachMethod.java
@@ -1,1 +1,16 @@
-
+// ForEach Method in java.
+import java.util.ArrayList;  
+import java.util.List;  
+public class ForEachExample {  
+    public static void main(String[] args) {  
+        List<string> l1 = new ArrayList<string>(); // Creating a new ArrayList to store games  
+        // Adding games to the list  
+        l1.add("Football");  
+        l1.add("Cricket");  
+        l1.add("Chess");  
+        l1.add("Hockey");  
+      
+        // Iterating over the list using forEach and printing each element using a lambda expression  
+        l1.forEach(game -> System.out.println(game));  
+    }  
+}  
