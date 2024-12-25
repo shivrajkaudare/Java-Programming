c871137bf961f5932acbae1a0e8bf5b4606a2041
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-12-25 21:37:38 +0530

    Update VectorExample.java

--- Collections/Vector/VectorExample.java
+++ Collections/Vector/VectorExample.java
@@ -1,1 +1,31 @@
+// The Vector class implements a growable array of objects. Vectors fall in legacy classes, but now it is fully compatible with collections. It is found in java.util package and implement the List interface.
+// Thread-Safe: All methods are synchronized, making it suitable for multi-threaded environments. However, this can lead to performance overhead in single-threaded scenarios.
+// Allows Nulls: Can store null elements.
+// Enumeration Support: Provides backward compatibility with Enumeration, a legacy way of iterating over elements.
 
+import java.util.Vector;
+
+public class VectorExample 
+{
+    public static void main(String[] args) 
+    {
+     
+        Vector<Integer> v = new Vector<>(3, 2);
+
+        // Add elements to the vector
+        v.addElement(1);
+        v.addElement(2);
+        v.addElement(3);
+
+        // Insert an element at index 1
+        v.insertElementAt(0, 1);
+
+        // Remove the element at index 2
+        v.removeElementAt(2);
+
+        // Print the elements of the vector
+        for (int i : v) {
+            System.out.println(i);
+        }
+    }
+}
