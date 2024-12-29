54d88d886a164fefa33c3b51c4b20b3b56166be7
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-12-29 19:12:46 +0530

    Update MinMaxInArray.java

--- Data-Structures/Array's/MinMaxInArray.java
+++ Data-Structures/Array's/MinMaxInArray.java
@@ -4,31 +4,21 @@
 
 public class MinMaxInArray {
     public static void main(String[] args) {
-        int[] arr = {3, 5, 1, 8, 2, -3, 7, 10};
-        
-        // Call the method and store results
-        int[] result = findMinMax(arr);
+        int[] arr = {3, 5, 1, 8, 2, -1, 10};
 
-        // Print the smallest and largest elements
-        System.out.println("Smallest Element: " + result[0]);
-        System.out.println("Largest Element: " + result[1]);
-    }
+        int min = arr[0];
+        int max = arr[0];
 
-    public static int[] findMinMax(int[] arr) {
-        // Initialize variables to store min and max
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-
-        // Iterate through the array
-        for (int num : arr) {
-            if (num < min) {
-                min = num; // Update min if a smaller value is found
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
             }
-            if (num > max) {
-                max = num; // Update max if a larger value is found
+            if (arr[i] > max) {
+                max = arr[i];
             }
         }
 
-        return new int[]{min, max}; // Return both min and max as an array
+        System.out.println("Minimum Element: " + min);
+        System.out.println("Maximum Element: " + max);
     }
 }
